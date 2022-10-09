f0c959c80de95784d44082ec433bb7120d92fe09
tarunraghav226 <tarun.raghav226@gmail.com>
2022-10-09 23:51:09 +0530

    Updated app.js file

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const WEATHER_API_KEY = 'fa736011e795194c2ec1cc14da5164e2'
-const WEATHER_API_DOMAIN = 'http://api.weatherstack.com/'
+geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
 
-const currentWeatherURL = `${WEATHER_API_DOMAIN}/current?access_key=${WEATHER_API_KEY}&query=28.7041,77.1025`
-
-request({
-    url: currentWeatherURL,
-    json: true
-}, (error, response)=>{
-    // console.log(response.body.current)
-    console.log(`It is currently ${response.body.current.temperature} but feels like ${response.body.current.feelslike}.`)
+forecast(44.1545, -75.7088, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
