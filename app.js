5718404da87cb82a9b01b188686978bc218252e1
tarunraghav226 <tarun.raghav226@gmail.com>
2022-10-10 00:05:54 +0530

    Added chaining

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+const address = process.argv[2]
+
+geocode(address, (error, data) => {
+    if(error){
+        return console.log(error)
+    }
+    
+    forecast(data.latitude, data.longitude, (forecastError, forecastData) => {
+        if(forecastError){
+            return console.log(error)
+        }
+    
+        console.log(data.location)
+        console.log(forecastData)
+    })
 })
 
-forecast(44.1545, -75.7088, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
