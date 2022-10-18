d80fa7ee99ef6ab9fe1bb199722db0ad45acbfd3
tarunraghav226 <tarun.raghav226@gmail.com>
2022-10-19 00:17:42 +0530

    Added server code to render hbs template file

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const express = require("express")
 
 const app = express()
 
+// Define paths for Express config
+const viewsPath = path.join(__dirname, './view')
 const staticAssestsPath = path.join(__dirname, "./public")
 app.use(express.static(staticAssestsPath))
+
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+app.get('/first', (req, res) => {
+    res.render('first',{
+        name: 'Tarun'
+    })
+})
 
 app.listen(3000, ()=>{
     console.log("Server is up on port 3000")
